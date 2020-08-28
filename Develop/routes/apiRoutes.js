43e240bfe70c1c2dfe224e3d472f056ec275d091
Gauri Khandke <khandkegauri@gmail.com>
2020-08-28 14:24:24 -0700

    Added API post request for new note

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,9 +5,32 @@ const notesData = require("../db/db.json");
 // ROUTING
 module.exports = function (app, fs) {
 
-    // API GET Requests
-    app.get("/api/notes", function(request, response){
+    // API GET Request
+    app.get("/api/notes", (request, response) => {
 
         response.send(notesData);
+    });
+
+    // API POST Request
+    app.post("/api/notes", (request, response) => {
+
+        // Extracted new note from request.  
+        const newNote = request.body;
+
+        // Pushed new note in notes file 'db.json'
+        notesData.push(newNote);
+
+        // Written notes data to 'db.json' file
+        fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
+            
+            if(err) throw(err);
+            
+            console.log("Successfully added new note to 'db.json' file!");
+
+        });
+
+        console.log("Notes : "+JSON.stringify(notesData));
+
+        response.json(newNote);
     });
 };
